4c4b13c96a55eb125e4bda77ce4e490651fed31e
Guillermo Alvarez <guillermo.alvarez@blackboard.com>
2020-05-02 19:43:43 -0500

    Updates

---
+++ precache-manifest.0e5abe0017d6ba3bcaa706dbb57405db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ddbacc7d10570a34852ffbd0cfa532",
+    "url": "/monster-cats/index.html"
+  },
+  {
+    "revision": "712dfbb57b3a3b89ef05",
+    "url": "/monster-cats/static/css/main.f7467060.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-cats/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-cats/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "712dfbb57b3a3b89ef05",
+    "url": "/monster-cats/static/js/main.8d531d77.chunk.js"
+  },
+  {
+    "revision": "73814d1b7c5d38350a32",
+    "url": "/monster-cats/static/js/runtime-main.318f707a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-cats/static/media/logo.5d5d9eef.svg"
+  }
+]);
